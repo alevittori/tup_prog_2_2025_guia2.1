33d3c6da5a1990ca5d26bd0e030803b69cc2c22b
alevittNote <alejandrovitt@hotmail.com>
2025-08-17 22:50:42 -0300

    iniciamos repositorio y terminamos de modelar las clases

---
+++ Guia_2.1/Ejercicio1/Models/DepartamentoVehicular.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    internal class DepartamentoVehicular
+    {
+        ArrayList registros = new ArrayList();
+
+        public int CantidadRegistros { get; private set; }
+        int nuevaSerie = 0;
+        string nuevaPatente = "";
+
+        public RegistroVehiculo RegistrarVechiculo(Persona elPropietario)
+        {
+            this.nuevaSerie = GenerarSerie();
+
+            this.nuevaPatente = GenerarPatente();
+
+            RegistroVehiculo unRegistro = new RegistroVehiculo(this.nuevaPatente, elPropietario, this.nuevaSerie);
+
+            return unRegistro;
+        }
+
+        string GenerarPatente()
+        {
+            string letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string patente = "";
+            Random rnd = new Random();
+            for (int i = 0; i < 2; i++)
+            {
+                patente += $"{letras[rnd.Next(letras.Length)]}";
+            }
+
+            //preguntar a que se refiere de forma secuencial, sino deberiamos cambiar esta formula
+            for(int i = 0;i < 3; i++)
+            {
+                patente += $"{rnd.Next(0, 10)}";
+            }
+            
+
+            return patente;
+        }
+        int GenerarSerie()
+        {
+            Random rnd = new Random();
+
+            string serie = "";
+            for (int i = 0; i < 9; i++)
+            {
+                serie += $"{rnd.Next(0, 9)}";
+            }
+
+            return int.Parse(serie);
+        }
+    }
+}
